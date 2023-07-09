710673b26fb099572cb1459b44d1e0875b9dc13d
Huseyin Erikli <herikli33@gmail.com>
2023-07-08 20:07:47 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductViewComponent } from './products/product-view/product-view.component';
+import { OrdersComponent } from './orders/orders.component';
+import { UsersComponent } from './users/users.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShipmentComponent } from './shipment/shipment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ProductListComponent,
+    ProductViewComponent,
+    OrdersComponent,
+    UsersComponent,
+    PaymentComponent,
+    ShipmentComponent
   ],
   imports: [
     BrowserModule
